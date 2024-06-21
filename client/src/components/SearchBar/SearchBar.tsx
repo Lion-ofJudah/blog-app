5f816805788e23e9f019ba6bf12bcfbec1a6a4c4
Metsakal Zeleke <MetsakalZ@gmail.com>
2024-06-21 17:34:39 +0300

    Create a search bar component.

---
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export default function SearchBar() {
+  const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <div className="flex items-center border px-2 py-2 rounded-full focus-within:border-red-500">
+      <span className={`${isFocused ? "hidden" : "block"} px-2`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="feather feather-search stroke-gray-300"
+        >
+          <circle cx="11" cy="11" r="8"></circle>
+          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+        </svg>
+      </span>
+      <input
+        type="text"
+        placeholder="Search"
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        className="px-2 py-2 focus-within:outline-none"
+      />
+      <div
+        className={`${
+          isFocused ? "block" : "hidden"
+        } p-2 rounded-full hover:bg-gray-200 cursor-pointer`}
+      >
+        <div className="rounded-full bg-black">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="feather feather-x stroke-neutral-50"
+          >
+            <line x1="18" y1="6" x2="6" y2="18"></line>
+            <line x1="6" y1="6" x2="18" y2="18"></line>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
